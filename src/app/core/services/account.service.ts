0c29d1332bc8302b5c41c4a1ab4486670f27bce6
nameis-duy <dphan2412@gmail.com>
2025-02-07 16:48:15 +0700

    add get account infor feature

--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -1,8 +1,18 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
+import { AppConstant } from "../constants/constant";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Account } from "../../shared/models/account.model";
+import { ResponseResult } from "../../shared/models/response.model";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AccountService {
+    private readonly API_URL = AppConstant.URL + "account";
+    private http = inject(HttpClient);
 
+    getAccountInfor() : Observable<ResponseResult<Account>> {
+        return this.http.get<ResponseResult<Account>>(this.API_URL + "/detail");
+    }
 }
--- src/app/features/account/account.component.ts
+++ src/app/features/account/account.component.ts
@@ -1,13 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { HeadersComponent } from "../../shared/components/headers/headers.component";
 import { SidebarComponent } from "../../shared/components/sidebar/sidebar.component";
+import { AccountService } from '../../core/services/account.service';
+import { Account } from '../../shared/models/account.model';
+import { ToastrService } from 'ngx-toastr';
+import { LoaderComponent } from "../../shared/components/loader/loader.component";
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account',
-  imports: [HeadersComponent, SidebarComponent],
+  imports: [CommonModule, HeadersComponent, SidebarComponent, LoaderComponent, ReactiveFormsModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
+  accountService = inject(AccountService);
+  toastr = inject(ToastrService);
+  accountInfor: Account | null = null;
+  accountInforForm?: FormGroup;
 
+  isLoading = signal<boolean>(false);
+  isUpdate = signal<boolean>(false);
+
+  constructor() {
+    this.initForm();
+  }
+
+  ngOnInit() {
+    this.getAccountInfor();
+  }
+
+  initForm(account?: Account) {
+    this.accountInforForm = new FormGroup({
+      firstName: new FormControl(account ? account.firstName : '', {
+        validators: [Validators.required, Validators.maxLength(250), Validators.minLength(1)],
+        nonNullable: true
+      }),
+      lastName: new FormControl(account ? account.lastName : '', {
+        validators: [Validators.required, Validators.maxLength(100), Validators.minLength(1)],
+        nonNullable: true
+      }),
+      email: new FormControl(account ? account.email : '', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: true
+      }),
+      phone: new FormControl(account ? account.phone : '', {
+        validators: [Validators.required],
+        nonNullable: true
+      }),
+      id: new FormControl(account?.id)
+    })
+  }
+
+  getAccountInfor() {
+    this.isLoading.set(true);
+    this.accountService.getAccountInfor().subscribe({
+      next: (res) => {
+        if (res.isSucceed) {
+          this.accountInfor = res.data;
+          this.initForm(this.accountInfor);
+        }
+        this.isLoading.set(false);
+      },
+      error: (err) => {
+        console.log("Error get account information:" + err);
+        this.isLoading.set(false);
+        this.toastr.error("Server error", "ERROR");
+      }
+    })
+  }
+
+  toggleUpdate(status: boolean) {
+    if (!status) {
+      this.initForm(this.accountInfor!);
+    }
+    this.isUpdate.set(status);
+  }
 }
